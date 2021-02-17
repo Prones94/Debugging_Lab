c87d4be41cf812c989fc848dd7dbc179a9ea02ba
Ian Rones <ronesp.85@gmail.com>
2021-02-17 10:50:16 -0800

    [add] new refactored methods completed

---
+++ SPD-2.31-Testing-and-Architecture/lab/refactoring/replace_temp_with_query_fowler.py
@@ -0,0 +1,13 @@
+def get_base_price(quantity, item_price):
+    base_price = quantity * item_price
+    return base_price
+def get_discount(base_price):
+    discount_factor = 0
+    if base_price > 1000:
+        discount_factor = 0.95
+    else:
+        discount_factor = 0.98
+    return discount_factor
+def get_total(base_price, discount_factor):
+    total = base_price * discount_factor
+    return total
